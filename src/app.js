f1458341bfd106574a82cdf9492bcf54773704ee
HarishkumarSP <61948549+HarishkumarSP@users.noreply.github.com>
2020-04-18 00:38:40 +0530

    FSJ0320B005

--- src/app.js
+++ src/app.js
@@ -22,12 +22,17 @@ const PLAYERS = [
 ];
 
 // Player Class
-class Player {
-    constructor(id, name, type) {
         // Create member variables and assign values
         // Type your code
-
+        class Player {
+            constructor(id, name, type) {
+                this.id = id;
+                this.name = name;
+                this.image = "images/super-" + (id + 1) + ".png";
+                this.strength = this.getRandomStrength();
+                this.type = type;
     }
+    
 
     // getting random strength
     getRandomStrength = () => {
@@ -35,22 +40,43 @@ class Player {
     }
 
     // Create a player for displaying
+    // Accumulate HTML template
+    // Type your code here
     view = () => {
-        // Accumulate HTML template
-        // Type your code here
-
-    
+        let player = document.createElement('div');
+        player.classList.add('player');
+        player.setAttribute('data-id', this.id);
+        if (this.selected == true)
+            player.classList.add('selected');
+        let image = document.createElement('img');
+        image.setAttribute('src', this.image);
+        let name = document.createElement('div');
+        name.textContent = this.name;
+        let strength = document.createElement('div');
+        strength.textContent = this.strength;
+        strength.className = 'strength';
+        player.append(image, name, strength);
         return player;
     }
 }
 
 // Superwar Class
-class Superwar {
-    constructor(players) {
     // Create a field players 
     // Use Map method to loop through players argument and create new players
     // Type your code here
 
+class Superwar {
+    constructor(players) {
+        this.players = players.map((player, i) => {
+            let type = (i % 2 == 0) ? 'hero' : 'villain';
+            return new Player(i, player, type);
+        });
+        this.score = [0, 0];
+        Array.from(document.getElementsByClassName('team'))
+            .forEach(elem => elem
+                .addEventListener('click', (e) => {
+                    this.handleSelection(e.target);
+                }));
     }
 
     // Display players in HTML
@@ -76,11 +102,10 @@ class Superwar {
             .forEach(player => fragment.append(player.view()));
         return fragment;
     }
-
 }
-
-
 window.onload = () => {
     const superwar = new Superwar(PLAYERS);
     superwar.viewPlayers();
 }
+
+
